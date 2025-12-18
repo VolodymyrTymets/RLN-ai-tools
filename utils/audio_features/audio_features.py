7fe7e9f8a3ffe6b33a655d8cba91dea99416407c
vova <volodymyrtymets@gmail.com>
2025-12-18 14:53:07 +0200

    add audio_features.py

---
+++ utils/audio_features/audio_features.py
@@ -0,0 +1,61 @@
+import librosa
+import numpy as np
+from scipy import signal as sp_signal
+
+
+class TimeDomainFeatures:
+
+  def __init__(self):
+    pass
+
+  def duration(self, signal: np.ndarray, sr: int):
+    return 1 / sr * len(signal)
+
+  def _frame(self, signal: np.ndarray, frame_length: int, hop_length: int):
+    return librosa.util.frame(signal, frame_length=frame_length, hop_length=hop_length, axis=0)
+
+  def amplitude_envelope(self, signal: np.ndarray, frame_length: int, hop_length: int):
+    return np.array([np.max(np.abs(frame)) for frame in self._frame(signal, frame_length, hop_length)])
+
+  def root_mean_square_energy(self, signal: np.ndarray, frame_length: int, hop_length: int):
+    frames = self._frame(signal, frame_length, hop_length)
+    return np.sqrt(np.mean(frames ** 2, axis=0))
+
+  def zero_crossing_rate(self, signal: np.ndarray, frame_length: int, hop_length: int):
+    return librosa.feature.zero_crossing_rate(signal, frame_length=frame_length, hop_length=hop_length)[0]
+
+  def AE(self, signal: np.ndarray, frame_length: int, hop_length: int):
+    return self.amplitude_envelope(signal=signal, frame_length=frame_length, hop_length=hop_length)
+
+  def RSME(self, signal: np.ndarray, frame_length: int, hop_length: int):
+    return self.root_mean_square_energy(signal=signal, frame_length=frame_length, hop_length=hop_length)
+
+  def ZCR(self, signal: np.ndarray, frame_length: int, hop_length: int):
+    return self.zero_crossing_rate(signal=signal, frame_length=frame_length, hop_length=hop_length)
+
+
+class FrequencyDomainFeatures:
+
+  def __init__(self):
+    pass
+
+  def _freq_for_magnitude(self, magnitude: np.array, sr: int):
+    return np.linspace(0, sr, len(magnitude))
+
+  def _magnitude(self, signal: np.array, f_ration: float = 0.5):
+    ft = np.fft.fft(signal)
+    magnitude = np.abs(ft)
+    nup_freq_bins = int(len(magnitude) * f_ration)
+    return magnitude[:nup_freq_bins]
+
+  def fft(self, signal: np.ndarray, sr: int, frame_length: int, hop_length: int):
+    # Step 1 framing
+    frames = librosa.util.frame(signal, frame_length=frame_length, hop_length=hop_length, axis=0)
+    # Step 2 windowing
+    window = sp_signal.windows.hamming(frame_length)
+    # Step 3 FFT
+    magnitudes = [self._magnitude(frame * window, 0.5) for frame in frames]
+    # Step 4 Aggregation
+    magnitude = np.mean(magnitudes, axis=0)
+    frequency = self._freq_for_magnitude(magnitude, sr)
+    return magnitude, frequency
---
+++ utils/plot.py
@@ -0,0 +1,99 @@
+import os
+import librosa, librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+from audio_features.audio_features import TimeDomainFeatures, FrequencyDomainFeatures
+
+
+DATASET_PATH = '../sanbox/assets'
+frame_length = 128
+hop_length = frame_length // 4
+
+b_1, sr = librosa.load(os.path.join(DATASET_PATH, 'breath', '1.wav'))
+b_2, _ = librosa.load(os.path.join(DATASET_PATH, 'breath', '2.wav'))
+b_3, _ = librosa.load(os.path.join(DATASET_PATH, 'breath', '3.wav'))
+
+n_1, _ = librosa.load(os.path.join(DATASET_PATH, 'nerve', '1.wav'))
+n_2, _ = librosa.load(os.path.join(DATASET_PATH, 'nerve', '2.wav'))
+n_3, _ = librosa.load(os.path.join(DATASET_PATH, 'nerve', '3.wav'))
+
+
+t_domain = TimeDomainFeatures()
+f_domain = FrequencyDomainFeatures()
+
+figsize = (7,3)
+
+## Amplitude envelope
+b_1_AE = t_domain.AE(signal=b_1, frame_length=frame_length, hop_length=hop_length)
+n_1_AE = t_domain.AE(signal=n_1, frame_length=frame_length, hop_length=hop_length)
+
+# plt.figure(figsize=figsize)
+# # ax = plt.subplot(1, 1, 1)
+# # #librosa.display.waveshow(b_1, color="black")
+# # plt.plot(librosa.frames_to_time(frames=range(len(b_1_AE)), hop_length=hop_length), b_1_AE, color="black")
+# # plt.ylim((-1, 1))
+# # plt.title("Breath")
+#
+# plt.subplot(1, 1, 1)
+# # librosa.display.waveshow(n_1, color="black")
+# plt.plot(librosa.frames_to_time(frames=range(len(n_1_AE)), hop_length=hop_length), n_1_AE, color="black")
+# plt.ylim((-1, 1))
+# plt.title("Stimulation")
+
+# plt.show()
+
+
+## Amplitude envelope
+b_1_RSME = t_domain.RSME(signal=b_1, frame_length=frame_length, hop_length=hop_length)
+n_1_RSME = t_domain.RSME(signal=n_1, frame_length=frame_length, hop_length=hop_length)
+
+# plt.figure(figsize=figsize)
+# plt.xlabel('Frames')
+# plt.ylabel('RMS Energy')
+# ax = plt.subplot(1, 1, 1)
+# plt.plot(b_1_RSME, color="black")
+# plt.title("Breath")
+#
+# # plt.subplot(1, 1, 1)
+# # plt.plot( n_1_RSME, color="black")
+# # plt.title("Stimulation")
+#
+# plt.show()
+
+
+## Zero crossing rate
+# b_1_ZCR = t_domain.ZCR(signal=b_1, frame_length=frame_length, hop_length=hop_length)
+# n_1_ZCR = t_domain.ZCR(signal=n_1, frame_length=frame_length, hop_length=hop_length)
+#
+# plt.figure(figsize=figsize)
+# plt.xlabel('Frames')
+# plt.ylabel('ZCR')
+#
+# # ax = plt.subplot(1, 1, 1)
+# # plt.plot(b_1_ZCR, color="black")
+# # plt.title("Breath")
+#
+# plt.subplot(1, 1, 1)
+# plt.plot( n_1_ZCR, color="black")
+# plt.title("Stimulation")
+#
+# plt.show()
+
+
+
+## spectr
+magnitude_b, frequency_b = f_domain.fft(signal=b_1, sr=sr, frame_length=frame_length, hop_length=hop_length)
+magnitude_n, frequency_n = f_domain.fft(signal=n_1, sr=sr, frame_length=frame_length, hop_length=hop_length)
+
+plt.figure(figsize=(7,4))
+# plt.xlabel('Frequency Hz')
+# plt.ylabel('Magnitude')
+# plt.plot( frequency_b, magnitude_b, color="black")
+# plt.title("Breath")
+
+plt.xlabel('Frequency Hz')
+plt.ylabel('Magnitude')
+plt.plot( frequency_n, magnitude_n, color="black")
+plt.title("Stimulation")
+
+plt.show()
